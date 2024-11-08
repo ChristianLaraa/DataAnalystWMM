77bc3831f5e6589fc7f19ed0b61dd4fbd4ccf8cf
Christian Lara <christian.lara.091204@gmail.com>
2024-11-08 00:25:33 -0600

    add prev data

--- predata/prefile.py
+++ predata/prefile.py
@@ -1,13 +1,7 @@
 import pandas as pd
 
-# Lee el archivo Excel. Cambia 'tus_datos.xlsx' por el nombre de tu archivo
-df = pd.read_excel('db.xlsx')
+# Lee el archivo Excel
+df = pd.read_excel('data.xlsx', sheet_name=None)  # Si el archivo tiene múltiples hojas
 
-# Convierte el DataFrame a JSON
-data_json = df.to_json(orient='records', indent=4, force_ascii=False)
-
-# Guarda el JSON en un archivo
-with open('data.json', 'w', encoding='utf-8') as json_file:
-    json_file.write(data_json)
-
-print("Conversión completada. Los datos se han guardado en 'data.json'")
+# Convierte el dataframe a JSON
+df.to_json('data.json', orient='records', lines=True)
