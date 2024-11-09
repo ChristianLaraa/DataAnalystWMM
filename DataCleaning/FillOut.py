dcd07d8c8ea0d2289bb8ad9e6597668fbeb538ce
Christian Lara <christian.lara.091204@gmail.com>
2024-11-08 21:45:05 -0600

    replace v2

--- DataCleaning/FillOut.py
+++ DataCleaning/FillOut.py
@@ -4,16 +4,14 @@ df = pd.read_csv("data.csv")
 
 df.fillna({
     '¿Con qué frecuencia juegas los siguientes tipos de videojuegos? [Role Playing Game]': 0,
-    '¿Con qué frecuencia juegas los siguientes tipos de videojuegos? [Acción]	': 0,
-    '¿Con qué frecuencia juegas los siguientes tipos de videojuegos? [Aventura]	': 0,
+    '¿Con qué frecuencia juegas los siguientes tipos de videojuegos? [Acción]': 0,
+    '¿Con qué frecuencia juegas los siguientes tipos de videojuegos? [Aventura]': 0,
     '¿Con qué frecuencia juegas los siguientes tipos de videojuegos? [Arcade]': 0,
     '¿Con qué frecuencia juegas los siguientes tipos de videojuegos? [Deportivo]': 0,
     '¿Con qué frecuencia juegas los siguientes tipos de videojuegos? [Estrategia]': 0,
-    '¿Con qué frecuencia juegas los siguientes tipos de videojuegos? [Simulación]	': 0,
+    '¿Con qué frecuencia juegas los siguientes tipos de videojuegos? [Simulación]': 0,
     '¿Con qué frecuencia juegas los siguientes tipos de videojuegos? [Juegos de mesa]': 0,
-    '¿Con qué frecuencia juegas los siguientes tipos de videojuegos? [Juegos musicales]	': 0,
-
-
+    '¿Con qué frecuencia juegas los siguientes tipos de videojuegos? [Juegos musicales]': 0,
 }, inplace=True)
 
 
